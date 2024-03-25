2f07858502b43345093b07e9433c5cd4e02ed080
LabyKreative <labykreative@yahoo.com>
2024-03-25 17:05:57 +0100

    Add @next/swc packages for different platforms

--- src/pages/portal.js
+++ src/pages/portal.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import Link from 'next/link';
-import Head from 'next/head';
-import Navbar from '@/Components/navbar/navbar';
-import Footer from '@/Components/Footer/footer';
-import Sign from '@/Components/SignUp/authentication';
+import React from "react";
+import Link from "next/link";
+import Head from "next/head";
+import Navbar from "@/Components/navbar/navbar";
+import Footer from "@/Components/Footer/footer";
+import Sign from "@/Components/SignUp/authentication";
 
 export default function Portal() {
   return (
     <>
-          <Head><title>Student Portal</title></Head>
-   
-<Navbar/>
-<div><Sign/></div>
-<Footer/>
+      <Head>
+        <title>Student Portal</title>
+      </Head>
+      <Navbar />
+      <div>
+        <Sign />
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
